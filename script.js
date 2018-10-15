6c0d3ff0d31dff003377c28de1c9f773c5d1d95e
James Wasson <jamesjameswasson@gmail.com>
2018-10-15 16:50:30 +0000

    Ajax=>http

--- script.js
+++ script.js
@@ -2,45 +2,29 @@
 
 angular.module('app', []).controller('mainCtrl', mainCtrl)
 
-function mainCtrl($scope)
-{
-    $scope.printRonQuote = function () 
-    {
+function mainCtrl($scope, $http) {
+    $scope.printRonQuote = function() {
         var myurl = "http://ron-swanson-quotes.herokuapp.com/v2/quotes/";
         var q = "\"";
         console.log(myurl);
-        $.ajax({
-            url: myurl,
-            dataType: "json",
-            success: function(parsed_json) {
-                console.log(parsed_json[0]);
-                q += parsed_json[0];
+        $http.get(myurl)
+            .then(function(response) {
+                q += response.data
                 console.log("q is: " + q);
                 q += "\" - Ron Swanson";
                 $scope.quote = q;
-                //$("#quote").html(q);
-            }
-        });
-        //$("#quote").html(q);
+            });
     }
-    
-    $scope.printQuote = function () 
-    {
-        $.ajax({
-            url: "https://talaikis.com/api/quotes/random/",
-            //crossDomain: true,
-            dataType: "json",
-            success: function(parsed_json) {
-                console.log(parsed_json);
-                var by = "-" + parsed_json['author'];
-                $scope.quote = parsed_json['quote'] + by;
-                //$('#quote').html(parsed_json['quote'] + by);
-            }
-        });
+
+    $scope.printQuote = function() {
+        $http.get("https://talaikis.com/api/quotes/random/")
+            .then(function(response) {
+                console.log(response);
+                var by = "-" + response.data['author'];
+                $scope.quote = response.data['quote'] + by;
+            });
     }
-    
-    $scope.displayPic = function () 
-    {
+    $scope.displayPic = function() {
         //console.log("here")
         var url = "https://picsum.photos/750/600/?image=";
         var int = Math.ceil(Math.random() * 1084);
@@ -48,9 +32,8 @@ function mainCtrl($scope)
         $scope.picture = url;
         $("#pic").css('visibility', 'visible');
     }
-    
-    $scope.surpriseMe = function() 
-    {
+
+    $scope.surpriseMe = function() {
         $("#para").css("color", "black");
         if (Math.random() < .5)
             $scope.printQuote();
